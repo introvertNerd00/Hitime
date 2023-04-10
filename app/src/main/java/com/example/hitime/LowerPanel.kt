818d7ce721ed1f722161521611ab9a2594310947
Hamza <hamza.afzalkhan9@gmail.com>
2023-04-10 22:58:48 +0500

    added lower panel class

---
+++ app/src/main/java/com/example/hitime/LowerPanel.kt
@@ -0,0 +1,32 @@
+package com.example.hitime
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LowerPanel() {
+    Column {
+        WeeklySpecial()
+        MenuDish()
+    }
+}
+
+
+@Composable
+fun WeeklySpecial(){
+    // to be defined
+}
+
+
+@Composable
+fun MenuDish() {
+    // to be defined
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LowerPanelPreview(){
+    LowerPanel()
+}
